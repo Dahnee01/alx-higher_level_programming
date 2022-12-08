4b74016a0ba3d80ce9114660c1a42db398d1d88c
Dahnee01 <mayodan21@gmail.com>
2022-12-08 08:42:19 +0100

    initail commit

--- 0x03-python-data_structures/tests/0-main.py
+++ 0x03-python-data_structures/tests/0-main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+from multiprocessing import get_logger
+
+
 print_list_integer = __import__('0-print_list_integer').print_list_integer
 
 my_list = [1, 2, 3, 4, 5]
 print_list_integer(my_list)
+get_loggergn
+
